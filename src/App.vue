9472c9e5ee0bdf1391febb014eb87357ab300be5
pegah <p.ahadian@gmail.com>
2020-08-13 18:39:26 +0430

    Employee form & list

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,69 @@
 <template>
-  <div id="app">
-    <HelloWorld />
+  <div id="app" class="small-container">
+    <img src="./assets/logo.png" alt="vue logo">
+    <h1>Login</h1>
+    <employee-table v-bind:employees="employees" @delete:employee="deleteEmployee"/>
+    <employeeform @add:employee="addEmployee" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import employeeform from './components/employeeform.vue';
+import EmployeeTable from '@/components/EmployeeTable.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    employeeform,
+    EmployeeTable
+  },
+  methods: {
+ addEmployee(employee) {
+  const lastId =
+    this.employees.length > 0
+      ? this.employees[this.employees.length - 1].id
+      : 0;
+  const id = lastId + 1;
+  const newEmployee = { ...employee, id };
+  this.employees = [...this.employees, newEmployee];
+},
+
+},
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Pegah Ahadian',
+          email: 'p.a@yahoo.com',
+        },
+        {
+          id: 2,
+          name: 'Test Test',
+          email: 'test@t.com',
+        },
+        {
+          id: 3,
+          name: 'Test2 Test2',
+          email: 'test2@t2.com',
+        },
+      ],
+    }
+  },
 }
+
 </script>
+<style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+  color: rgb(71, 71, 71);
+}
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+  .small-container {
+    max-width: 680px;
+  }
+</style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee Name</th>
+          <th>Employee Email</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+          <td>
+            <button>Edit</button>
+            <button>Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'employee-table',
+      props: {
+    employees: Array,
+  },
+  }
+</script>
+<style scoped>
+  button {
+    margin: 0 0.5rem 0 0;
+    background-color: rgba(32, 32, 204, 0.815);
+  }
+</style>
